da48f54c3fab0139fdf268491602faaaea7f31d6
Vibhor Agarwal <agarwalvibhor930@gmail.com>
2024-05-29 19:05:49 +0530

    UserSignUp extracting data and validating at the backend

---
+++ actions/auth-actions.js
@@ -0,0 +1,22 @@
+"use server";
+
+export async function signup(prevSTate, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  let errors = {};
+
+  if (!email.includes("@")) {
+    errors.email = "Invalid email";
+  }
+  if (password.trim().length < 8) {
+    errors.password = "Password must be at least 8 characters long";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return {
+      errors,
+    };
+  }
+  //store in db
+}
